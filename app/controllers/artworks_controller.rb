d24df6231711d12729c657afd4f73def843457f8
coreybarker <cozybarker@gmail.com>
2022-05-11 11:36:18 +1000

    add artwork via view added

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -7,12 +7,22 @@ class ArtworksController < ApplicationController
 
     def show; end
 
+    def new
+        @artwork = Artwork.new
+    end
+
     def create
+        artwork = Artwork.create(artwork_params)
+        redirect_to artwork
     end
 
   private
 
     def find_artwork
         @artwork = Artwork.find(params[:id])
+    end
+
+    def artwork_params
+        return params.require(:artwork).permit(:title, :artist, :description, :price)
     end
 end
