59a84f72951c61c1fe1a5d373dc1edd5442a266e
coreybarker <cozybarker@gmail.com>
2022-05-13 14:55:18 +1000

    login authentication added for users

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,5 @@
 class ArtworksController < ApplicationController
+    before_action :authenticate_user!, except: %i[index]
     before_action :find_artwork, only: %i[show edit update destroy]
 
     def index
