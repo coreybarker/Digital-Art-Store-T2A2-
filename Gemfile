85a56afde0cc89f748457b59711177be88773728
coreybarker <cozybarker@gmail.com>
2022-05-13 10:47:48 +1000

    user model added with devise

--- Gemfile
+++ Gemfile
@@ -82,3 +82,5 @@ gem "activestorage-cloudinary-service", "~> 0.2.3"
 
 
 gem "pg", "~> 1.3", :group => :production
+
+gem "devise", "~> 4.8"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :artworks
   # Defines the root path route ("/")
   # root "articles#index"
