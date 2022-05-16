50d087745190f918a821a2e31df8d190da4cc5f8
coreybarker <cozybarker@gmail.com>
2022-05-16 15:08:12 +1000

    seeds file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Artwork.destroy_all
 
-Artwork.create(title: 'Test1', artist: 'Banksy', price: '2500', description: 'One of a kind piece')
-Artwork.create(title: 'Pixel Painting', artist: 'Piccaso', price: '600', description: 'A legendary piccaso pixel painting')
-Artwork.create(title: 'Sunshine', artist: 'James', price: '150', description: 'A worldly sunset image')
+Artwork.create(title: 'Pixel Painting', artist: 'Leonardo da Vinci', price: '600', description: 'A legendary da Vinci pixel painting')
+Artwork.create(title: 'Sunshine', artist: 'Efe Kurnaz', price: '150', description: 'A worldly sunset image')
+Artwork.create(title: 'Something', artist: 'Erick Butler', price: '80', description: 'Artwork of something')
+Artwork.create(title: 'Holographic', artist: 'Simon Lee', price: '500', description: 'One of a kind piece')
+Artwork.create(title: 'Color Streaks', artist: 'Omid Armin', price: '600', description: 'Some color amongst the shadows')
+Artwork.create(title: 'Moving', artist: 'Sketch Machine', price: '200', description: 'Moving Artwork')
+Artwork.create(title: 'Purple Chaos', artist: 'Callum Broarde', price: '380', description: 'Chaos in motion')
